7575a7495a3852e61f26f362b350c90fd2a73329
Wanni64 <29074256+Wanni64@users.noreply.github.com>
2019-10-21 22:22:36 +0200

    Stopped multiple sounds from playing at once

--- SoundBoard/Form1.cs
+++ SoundBoard/Form1.cs
@@ -6,6 +6,7 @@ namespace SoundBoard
 {
     public partial class Form1 : Form
     {
+        bool isPlaying = false;
         public Form1()
         {
             InitializeComponent();
@@ -18,50 +19,98 @@ namespace SoundBoard
 
         private void Crickets_Click(object sender, EventArgs e)
         {
+            if(isPlaying) {
+                return;
+            }
             SoundPlayer cricketSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_cricket.wav");
+            isPlaying = true;
+            isPlaying = true;
             cricketSound.Play();
+            isPlaying = false;
         }
 
         private void Frog_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer frogSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_frog.wav");
+            isPlaying = true;
             frogSound.Play();
+            isPlaying = false;
         }
 
         private void Disastah_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer disastahSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_disastah.wav");
+            isPlaying = true;
             disastahSound.Play();
+            isPlaying = false;
         }
 
         private void CryBaby_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer cryBabySound = new SoundPlayer(@"..\..\Sounds\Chatwheel_crybaby.wav");
+            isPlaying = true;
             cryBabySound.Play();
+            isPlaying = false;
         }
 
         private void Patience_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer patienceSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_patience.wav");
+            isPlaying = true;
             patienceSound.Play();
+            isPlaying = false;
         }
 
         private void Brutal_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer brutalSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_brutal.wav");
+            isPlaying = true;
             brutalSound.Play();
+            isPlaying = false;
         }
 
         private void Ehto_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer ehtoSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_ehto_g_g.wav");
+            isPlaying = true;
             ehtoSound.Play();
+            isPlaying = false;
         }
 
         private void AllDead_Click(object sender, EventArgs e)
         {
+            if (isPlaying)
+            {
+                return;
+            }
             SoundPlayer allDeadSound = new SoundPlayer(@"..\..\Sounds\Chatwheel_all_dead.wav");
+            isPlaying = true;
             allDeadSound.Play();
+            isPlaying = false;
         }
     }
 }
